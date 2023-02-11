43e118d335f2cd99dd4875cf54df901982108541
Rakshith u r <20pw27@psgtech.ac.in>
2023-02-11 13:22:30 +0530

    gui

---
+++ gui/src/App.js
@@ -0,0 +1,37 @@
+
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+  const [marketId, setMarketId] = useState('');
+  const [score, setScore] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // Call an API endpoint to retrieve the data for the specified market ID
+      const data = await fetch(`/api/market?id=${marketId}`).then(res => res.json());
+
+      // Calculate the score using the given attributes
+      // const calculatedScore = calculateScore(data);
+
+      // Set the score state to the calculated score
+      setScore(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="marketId">Market ID:</label>
+        <input type="text" id="marketId" value={marketId} onChange={e => setMarketId(e.target.value)} />
+        <button type="submit">Calculate Score</button>
+      </form>
+      {score && <div>Score: {score}</div>}
+    </div>
+  );
+}
+
+export default App;
